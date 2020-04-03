79d43dcab71736cfae5b8c7a6911ee0f2c3e2632
klozz <klozz@vivaldi.net>
2020-04-03 10:33:31 +0000

    m1852: track perf blobs from polaris miui 11.04
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -224,6 +224,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+TARGET_COMMON_QTI_COMPONENTS := \
+    perf
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
