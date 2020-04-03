987697223f05927ab78f362a505f0fa75493e7e0
klozz <klozz@vivaldi.net>
2020-04-03 10:13:23 +0000

    m1852: Add more hidl files
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -158,7 +158,11 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
+    android.hidl.manager@1.0-java
 
 # Init
 PRODUCT_COPY_FILES += \
