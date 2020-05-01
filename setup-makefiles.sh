ecbff72b28369f9a5b66f0fbb26964c9101c8868
klozz <klozz@vivaldi.net>
2020-05-01 09:09:45 +0000

    m1852: update setup-makefiles.sh
    Signed-off-by: klozz <klozz@vivaldi.net>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #
 # Copyright (C) 2020 The MoKee Open Source Project
+# Copyright (C) 2020 The XPerience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
+
+DEVICE=m1852
+VENDOR=meizu
 
 INITIAL_COPYRIGHT_YEAR=2020
 
@@ -13,9 +17,9 @@ INITIAL_COPYRIGHT_YEAR=2020
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-MOKEE_ROOT="${MY_DIR}/../../.."
+XPERIENCE_ROOT="${MY_DIR}/../../.."
 
-HELPER="${MOKEE_ROOT}/vendor/mokee/build/tools/extract_utils.sh"
+HELPER="${XPERIENCE_ROOT}/vendor/xperience/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -23,28 +27,14 @@ fi
 source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${MOKEE_ROOT}" true
+setup_vendor "$DEVICE" "$VENDOR" "$XPERIENCE_ROOT" true
 
 # Copyright headers and guards
-write_headers "m1852 m1872"
+write_headers
 
+write_makefiles "${MY_DIR}/proprietary-files-m1852.txt"
 write_makefiles "${MY_DIR}/proprietary-files.txt"
 write_makefiles "${MY_DIR}/proprietary-files-qc.txt"
 
 # Finish
 write_footers
-
-if [ -f "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="${DEVICE_BRINGUP_YEAR}"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${MOKEE_ROOT}" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt"
-
-    # Finish
-    write_footers
-fi
