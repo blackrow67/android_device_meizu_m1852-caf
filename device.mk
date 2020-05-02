eb40ef1e031e76dfa0dee9f4c3418c6253860205
klozz <klozz@vivaldi.net>
2020-05-02 11:18:19 +0000

    m1852: build libashmemd_client
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -105,6 +105,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libshim_camera \
+    libashmemd_client \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
