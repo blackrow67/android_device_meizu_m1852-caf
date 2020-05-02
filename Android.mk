4f8f5cd5c773dacf1440863f392b921a24424dc3
klozz <klozz@vivaldi.net>
2020-05-02 10:22:17 +0000

    m1852: Move NoCutoutOverlay to root folder * For some strange reason the makefile for "NoCutOutOverlay" was not read in that location .----.
    Signed-off-by: klozz <klozz@vivaldi.net>

--- Android.mk
+++ Android.mk
@@ -7,11 +7,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),m1852)
+ifneq ($(filter m1852,$(TARGET_DEVICE)),)
 
-  include $(call all-makefiles-under,$(LOCAL_PATH))
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+include $(call all-makefiles-under,$(LOCAL_PATH))
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 include $(CLEAR_VARS)
 
